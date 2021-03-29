f97696576ea76414e6344283f09946afc427407d
Oleg Irchishyn <o.irchishyn@smileexpo.com.ua>
2021-03-29 15:33:26 +0300

    updating project

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,21 +1,66 @@
 import React from 'react';
 import styles from './../../styles/components/Sidebar.module.scss';
-import ListSvg from '../../assets/images/list.svg';
 import cn from 'classnames';
+const Sidebar: React.FC = React.memo(() => {
+  const items = [
+    {
+      icon: (
+        <svg
+          width="18"
+          height="18"
+          viewBox="0 0 18 18"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M12.96 8.10001H7.74001C7.24321 8.10001 7.20001 8.50231 7.20001 9.00001C7.20001 9.49771 7.24321 9.90001 7.74001 9.90001H12.96C13.4568 9.90001 13.5 9.49771 13.5 9.00001C13.5 8.50231 13.4568 8.10001 12.96 8.10001V8.10001ZM14.76 12.6H7.74001C7.24321 12.6 7.20001 13.0023 7.20001 13.5C7.20001 13.9977 7.24321 14.4 7.74001 14.4H14.76C15.2568 14.4 15.3 13.9977 15.3 13.5C15.3 13.0023 15.2568 12.6 14.76 12.6ZM7.74001 5.40001H14.76C15.2568 5.40001 15.3 4.99771 15.3 4.50001C15.3 4.00231 15.2568 3.60001 14.76 3.60001H7.74001C7.24321 3.60001 7.20001 4.00231 7.20001 4.50001C7.20001 4.99771 7.24321 5.40001 7.74001 5.40001ZM4.86001 8.10001H3.24001C2.74321 8.10001 2.70001 8.50231 2.70001 9.00001C2.70001 9.49771 2.74321 9.90001 3.24001 9.90001H4.86001C5.35681 9.90001 5.40001 9.49771 5.40001 9.00001C5.40001 8.50231 5.35681 8.10001 4.86001 8.10001ZM4.86001 12.6H3.24001C2.74321 12.6 2.70001 13.0023 2.70001 13.5C2.70001 13.9977 2.74321 14.4 3.24001 14.4H4.86001C5.35681 14.4 5.40001 13.9977 5.40001 13.5C5.40001 13.0023 5.35681 12.6 4.86001 12.6ZM4.86001 3.60001H3.24001C2.74321 3.60001 2.70001 4.00231 2.70001 4.50001C2.70001 4.99771 2.74321 5.40001 3.24001 5.40001H4.86001C5.35681 5.40001 5.40001 4.99771 5.40001 4.50001C5.40001 4.00231 5.35681 3.60001 4.86001 3.60001Z"
+            fill="#000"
+          />
+        </svg>
+      ),
+      name: 'All Tasks',
+      active: true,
+    },
+    {
+      color: '#42B883',
+      name: 'Purchases',
+    },
+    {
+      color: '#64C4ED',
+      name: 'Front-end',
+    },
+    {
+      color: '#FFBBCC',
+      name: 'TV films and series',
+    },
+    {
+      color: '#B6E6BD',
+      name: 'Books',
+    },
+    {
+      color: '#C9D1D3',
+      name: 'Private',
+    },
+  ];
 
-const Sidebar = React.memo(() => {
   return (
     <div className={cn(styles.todo__sidebar)}>
       <ul className={cn(styles.todo__sidebar_list)}>
-        <li
-          className={cn({
-            [styles.active]: false,
-          })}>
-          <i>
-            <img src={ListSvg} alt="list icon" />
-          </i>
-          <span>All tasks</span>
-        </li>
+        {items.map((item, index) => (
+          <li
+            key={index}
+            className={cn({
+              [styles.active]: item.active && item.active ? true : false,
+            })}>
+            <i>
+              {item.icon ? (
+                item.icon
+              ) : (
+                <i style={{ background: `${item.color}` }} className={styles.badge}></i>
+              )}
+            </i>
+            <span>{item.name}</span>
+          </li>
+        ))}
       </ul>
     </div>
   );
